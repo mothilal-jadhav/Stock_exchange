51584a35cb55c330192e92024853133c4447d952
Mothilal Jadhav <mothilaljadav0304@gmail.com>
2026-07-07 14:40:51 +0530

    essential files for project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StockExchange
+//
+//  Created by Mothilal Jadhav on 07/07/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
